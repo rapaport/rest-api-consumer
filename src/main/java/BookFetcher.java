e431d3b3a3c2d5e67deb7e5f09520d4a83770063
rapaport <rapaport@gmail.com>
2023-12-11 12:06:32 +0100

    structure for response the OpenLibrary Subject API gives

---
+++ src/main/java/BookFetcher.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class BookFetcher {
+    private final WebApiFetcher webApiFetcher = new WebApiFetcher();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public void getBookList (String subject, int startYear, int endYear, int maxResults) throws IOException {
+        String apiUrl = "https://openlibrary.org/subjects/" + subject + ".json?published_in=" + startYear + "-" + endYear + "&limit="+ maxResults;
+        String booksResult = webApiFetcher.fetchDataFromApi(apiUrl);
+        System.out.println(booksResult);
+
+    }
+
+
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,40 +12,44 @@ public class Main {
     {
         try {
 
+            BookFetcher bookFetcher = new BookFetcher();
+            bookFetcher.getBookList("mid-century", 2005, 2023, 30);
+            //BookFetcher anotherBookFetcher = new BookFetcher();
+
             // activities
-            ActivityFetcher activityFetcher = new ActivityFetcher();
-
-//          Activity activity = activityFetcher.getRandomActivity();
-            Activity activity = activityFetcher.getActivityByType("recreational");
-
-            System.out.println("Activity: " + activity.getActivity());
-            System.out.println("Type: " + activity.getType());
-            if (!activity.getLink().isBlank()) {
-                System.out.println("Link: " + activity.getLink());
-            }
-
-            // jokes
-            JokeFetcher jokeFetcher = new JokeFetcher();
-            Joke randomJoke = jokeFetcher.getRandomJoke();
-            System.out.println();
-            System.out.println(randomJoke.getSetup());
-            System.out.println(randomJoke.getPunchline());
-
-            List<Joke> randomTen = jokeFetcher.getRandomTen();
-            for (response.Joke joke : randomTen) {
-                System.out.println();
-                System.out.println(joke.getSetup());
-                System.out.println(joke.getPunchline());
-            }
-
-            // users
-            UserFetcher userFetcher = new UserFetcher();
-            UserResponse userResponse = userFetcher.getRandomUser();
-            for (User user : userResponse.getResults()) {
-                System.out.println();
-                System.out.println("Name: " + user.getName().getFirst());
-                System.out.println("Gender: " + user.getGender());
-            }
+//            ActivityFetcher activityFetcher = new ActivityFetcher();
+//
+////          Activity activity = activityFetcher.getRandomActivity();
+//            Activity activity = activityFetcher.getActivityByType("recreational");
+//
+//            System.out.println("Activity: " + activity.getActivity());
+//            System.out.println("Type: " + activity.getType());
+//            if (!activity.getLink().isBlank()) {
+//                System.out.println("Link: " + activity.getLink());
+//            }
+//
+//            // jokes
+//            JokeFetcher jokeFetcher = new JokeFetcher();
+//            Joke randomJoke = jokeFetcher.getRandomJoke();
+//            System.out.println();
+//            System.out.println(randomJoke.getSetup());
+//            System.out.println(randomJoke.getPunchline());
+//
+//            List<Joke> randomTen = jokeFetcher.getRandomTen();
+//            for (response.Joke joke : randomTen) {
+//                System.out.println();
+//                System.out.println(joke.getSetup());
+//                System.out.println(joke.getPunchline());
+//            }
+//
+//            // users
+//            UserFetcher userFetcher = new UserFetcher();
+//            UserResponse userResponse = userFetcher.getRandomUser();
+//            for (User user : userResponse.getResults()) {
+//                System.out.println();
+//                System.out.println("Name: " + user.getName().getFirst());
+//                System.out.println("Gender: " + user.getGender());
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
