788cac9a9844eb3d15575becfcb0d18b5eb1b637
rapaport <1638550+rapaport@users.noreply.github.com>
2023-12-11 12:16:20 +0100

    Add files via upload
    OpenLibrary API reply

---
+++ Root.java
@@ -0,0 +1,9 @@
+package response;
+import java.util.ArrayList;
+public class Root{
+    public String key;
+    public String name;
+    public String subject_type;
+    public int work_count;
+    public ArrayList<Work> works;
+}
